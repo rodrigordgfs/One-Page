842f155f75d6272fc235f67cf0b53b2624494113
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2021-07-18 21:50:33 -0300

    Firt Commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Scrool Reveal Effect
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "50px",
+    duration: 2000,
+    reset: true,
+  });
+  
+  ScrollReveal().reveal(".card", { delay: 200 });
+  
